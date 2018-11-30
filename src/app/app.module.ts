d79e694691e77eab6eb51163d5207a08c952fc34
Carlos <carlosmm147@gmail.com>
2018-11-30 14:05:32 -0600

    fin de curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,22 @@ import { MapaComponent } from './components/mapa/mapa.component';
 
 /* Google maps */
 import { AgmCoreModule } from '@agm/core';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
+  entryComponents: [MapaEditarComponent],
   declarations: [
     AppComponent,
-    MapaComponent
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
+    ReactiveFormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCroCERuudf2z02rCrVa6DTkeeneQuq8TA'
     })
--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { InfoWindowManager } from '@agm/core';
 import { Marcador } from '../../classes/marcador.class';
+import { log } from 'util';
+import {MatSnackBar} from '@angular/material';
+import {MatDialog, MatDialogRef } from '@angular/material';
+import { MapaEditarComponent } from './mapa-editar.component';
 
 @Component({
   selector: 'app-mapa',
@@ -15,15 +19,26 @@ export class MapaComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() {
+  constructor( public snackBar: MatSnackBar,
+               public dialog: MatDialog     ) {
 
     const nuevoMarcador = new Marcador(51.678418, 7.80900);
     this.marcadores.push( nuevoMarcador );
+
+    if ( localStorage.getItem('marcadores') ) {
+      this.marcadores = JSON.parse( localStorage.getItem('marcadores') );
+    }
 
 
    }
 
   ngOnInit() {
+  }
+
+  borrarMarcador(i) {
+    this.marcadores.splice(i, 1);
+    this.saveStorage();
+    this.snackBar.open('Marcador borrado', 'Cerrar', {duration: 3000});
   }
 
   agregarMarcador(evento) {
@@ -34,7 +49,36 @@ export class MapaComponent implements OnInit {
 
     this.marcadores.push( nuevoMarcador );
 
+    console.log(this.marcadores);
 
+    this.saveStorage();
+    this.snackBar.open('Marcador agregado', 'Cerrar', {duration: 3000});
+  }
+
+  editarMarcador( marcador: Marcador ) {
+        const dialogRef = this.dialog.open(MapaEditarComponent, {
+          width: '250px',
+          data: { titulo: marcador.titulo , desc: marcador.desc }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+          console.log(result);
+
+          if (!result) {
+            return;
+          }
+
+          marcador.titulo = result.titulo;
+          marcador.desc = result.desc;
+
+          this.saveStorage();
+          this.snackBar.open('Marcador actualizado', 'Cerrar', {duration: 3000});
+
+        });
+  }
+
+  saveStorage() {
+    localStorage.setItem('marcadores', JSON.stringify( this.marcadores ) );
   }
 
 }
